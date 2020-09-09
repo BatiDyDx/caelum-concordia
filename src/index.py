6b40416fa366471d21634f4680ab7b4291d9d7d1
Bautista Peirone <bautista.peirone@gmail.com>
2020-09-09 00:35:11 -0300

    Added .gitignore and other features, ready for first deployment

--- src/index.py
+++ src/index.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'#sql10.freemysqlhosting.net
-app.config['MYSQL_USER'] = 'root'#sql10364048
-app.config['MYSQL_PASSWORD'] = ''#RYuAGkygdA
-app.config['MYSQL_DB'] = 'Caelum'#sql10364048
+app.config['MYSQL_HOST'] = 'localhost' #sql10.freemysqlhosting.net , mysql-12563-0.cloudclusters.net
+app.config['MYSQL_USER'] = 'root' #sql10364048 , Bati
+app.config['MYSQL_PASSWORD'] = '' #RYuAGkygdA , Bautista01
+app.config['MYSQL_DB'] = 'Caelum' #sql10364048 , Caelum
 mysql = MySQL(app)
+
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def home():
@@ -15,6 +17,25 @@ def home():
     cur.execute('SELECT * FROM products ORDER BY product_id DESC LIMIT 6')
     data = cur.fetchall()
     return render_template('home.html', products = data)
+
+@app.route('/add_suscriber', methods=['POST'])
+def add_suscriber():
+    if request.method == 'POST':
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (user_email) VALUES('{0}')".format(email))
+        mysql.connection.commit()
+        flash('Muchas gracias por suscribirte')
+        return redirect(url_for('home'))
+
+@app.route('/delete_suscriber', methods=['POST'])
+def delete_suscriber():
+    if request.method == 'POST':
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM users WHERE user_email = '{0}'".format(email))
+        mysql.connection.commit()
+        return redirect(url_for('home'))
 
 @app.route('/about')
 def about():
